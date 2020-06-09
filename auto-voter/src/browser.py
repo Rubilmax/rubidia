9b300e947dabcb9aeb39a074d0cb8fb76784b8cf
Rubilmax <rmilon@gmail.com>
2020-06-09 18:40:26 +0200

    Fixed chromdriver path

--- auto-voter/src/browser.py
+++ auto-voter/src/browser.py
@@ -5,7 +5,7 @@ from selenium.webdriver.remote.remote_connection import LOGGER
 LOGGER.setLevel(logging.WARNING)
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
-executable_path = "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe"
+executable_path = "/usr/bin/chromedriver"
 
 
 def get_driver(proxy=""):
